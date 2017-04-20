41400b214520c460903983d4492060212e7a939c
Girish Narayanswamy <girish.narayanswamy@gmail.com>
2017-04-19 21:43:48 -0600

    created fetch status files

---
+++ fetch.h
@@ -0,0 +1,14 @@
+//
+// Created by Girish Narayanswamy on 4/19/17.
+//
+
+#ifndef ECEN4593_MIPS_EMULATOR_FETCH_H
+#define ECEN4593_MIPS_EMULATOR_FETCH_H
+
+
+class fetch {
+
+};
+
+
+#endif //ECEN4593_MIPS_EMULATOR_FETCH_H
